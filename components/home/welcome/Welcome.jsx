fc03c732798a579e95361ba7c797a693a8638b39
Crucialjun <nich.otieno@gmail.com>
2023-08-16 09:30:38 +0300

    update

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, TextInput } from "react-native";
 
 import styles from "./welcome.style";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { icons } from "../../../constants";
 
 const Welcome = () => {
   return (
@@ -12,8 +14,20 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}></TextInput>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for? "
+          ></TextInput>
         </View>
+        <TouchableOpacity>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          ></Image>
+        </TouchableOpacity>
       </View>
     </View>
   );
